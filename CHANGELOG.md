3f289e4d3d1840f7d32b5112882ecb4059b90e92
Kyle Allan <kyle@stitchdata.com>
2021-01-22 17:08:24 +0000

    bump to 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 0.0.2
+  * Replication keys now receive automatic inclusion metadata [#2](https://github.com/singer-io/tap-lookml/pull/2)
+
 ## 0.0.1
   * Initial commit
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-lookml',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting metadata from LookML files with the GitHub API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
